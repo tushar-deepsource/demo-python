3ac24d249e05bdbb30e7160c549c1274dc07b9ef
Tushar Sadhwani <tushar@deepsource.io>
2022-01-10 19:52:00 +0530

    Update demo code

--- more_demo_code.py
+++ more_demo_code.py
@@ -1,11 +1,10 @@
-import unittest.mock
+import urllib.parse
 import random
 
-import unittest
+from urllib.parse import urlparse
 
 # import time
-# import unittest
-
+# import timeit
 
 value = input("> ")
 if value == "red" or value == "blue":
@@ -24,6 +23,12 @@ good_digits.add(15)
 params = {"HOST": "127.0.0.1", "PORT": 8000}
 params["User-agent"] = "Mozilla/5.0"
 
+logo = (
+    " __   ___  ___  __   __   __        __   __   ___ \n"
+    "|  \ |__  |__  |__) /__` /  \ |  | |__) /  ` |__  \n"
+    "|__/ |___ |___ |    .__/ \__/ \__/ |  \ \__, |___ \n"
+)
+
 
 def is_bad(item):
     return "bad" in item
@@ -40,5 +45,16 @@ def func():
 for item in items:
     if is_bad(item):
         all_good_values = False
-else:
-    all_good_values = True
+    
+all_good_values = True
+
+
+url = urlparse("https://google.com/?q=deepsource+python")
+if url.hostname == "google.com" or url.hostname == "github.com":
+    print("OAuth provider detected")
+
+if urllib.parse.ParseResult == type(url):
+    print(url.scheme)
+
+evens = set([num for num in range(10) if num % 2 == 0])
+assert 8 in evens
