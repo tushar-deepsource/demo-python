268b31b11d17a7cd540ae3793dec7deaedb589b7
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-03-08 20:07:45 +0530

    Add W0053

--- demo_code.py
+++ demo_code.py
@@ -8,3 +8,18 @@ def f():
 
     z
     """
+
+class MyAbstractClass:
+    """Test"""
+    @abc.abstractproperty
+    def myproperty(self):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def myproperty2(cls):
+        pass
+
+    @abc.abstractmethod
+    def mymethod(self):
+        pass
