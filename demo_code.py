455835d46ecec11e26e1409a006c87d0afa14910
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-06-27 17:29:53 +0530

    test

--- demo_code.py
+++ demo_code.py
@@ -27,3 +27,19 @@ class MyAbstractClass:
     @abc.abstractmethod
     def mymethod(self):
         pass
+
+def foo():
+    """
+    This docstring is 80 characters long, normally flake8 would raise on this.
+    """
+
+def bar():
+    """
+    This docstring is 90 characters long, still no issue as we set the max line length to 100.
+    """
+
+
+def baz():
+    """
+    This docstring is 105 characters long, now that is an issue, this is longer than max line length so raise here.
+    """
