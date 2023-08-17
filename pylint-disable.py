fd6452808504ca2535899f31f81e3d7222ffd9c5
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-08-17 16:12:25 +0530

    Update pylint-disable.py

--- pylint-disable.py
+++ pylint-disable.py
@@ -6,26 +6,28 @@ try:
 except Exception: # noqa: B902 # pylint: disable=broad-except
     print("caught")
 
-def foo():
-    raise RuntimeError
-
 try:
     foo()
 except Exception: # noqa: B902 # skipcq: PYL-W0703
     print("caught")
-
-def foo():
-    raise RuntimeError
 
 try:
     foo()
 except Exception: # noqa: B902 # pylint: disable
     print("caught")
 
-def foo():
-    raise RuntimeError
-
 try:
     foo()
 except Exception: # noqa: B902 # skipcq
     print("caught")
+
+class Foo:
+    class RelatedModel(Model):  # pylint: disable=unused-variable # skipcq: PTC-W0065
+        """Something"""
+        def foo(self):
+            print("foo")
+
+    class AnotherModel(Model):  # pylint: disable=unused-variable # skipcq: PTC-W0065
+        """Something"""
+        def bar(self):
+            print("bar")
