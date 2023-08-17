002fc144a74095c3a594aa4e58cb985d8cc5bf00
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-08-17 16:01:29 +0530

    Update pylint-disable.py

--- pylint-disable.py
+++ pylint-disable.py
@@ -3,5 +3,13 @@ def foo():
 
 try:
     foo()
-except Exception:  # pylint: disable=broad-except # noqa
+except Exception: # noqa # pylint: disable=broad-except
+    print("caught")
+
+def foo():
+    raise RuntimeError
+
+try:
+    foo()
+except Exception: # noqa # skipcq: PYL-W0703
     print("caught")
