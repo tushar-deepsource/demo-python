73810e0058de7818b1b6f3913e8beb13b86a704a
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-09-21 14:28:54 +0530

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -18,6 +18,15 @@ class MyAbstractClass:
     def myproperty(self):
         pass
 
+    def some_long_method(
+        attribute_1: str | None = None,
+        attribute_2: str | None = None,
+        attribute_3: str | None = None,
+        attribute_4: str | None = None,
+        attribute_5: str | None = None,
+    ):
+        pass
+    
     @property
     @abc.abstractmethod
     def myproperty2(cls):
