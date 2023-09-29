a181eaf4a846ddef0118ddaf32cc3be915a084f5
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-09-29 14:45:30 +0530

    Update django_issues.py

--- django_issues.py
+++ django_issues.py
@@ -11,3 +11,6 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is %s.</body></html>" % now
     return HttpResponse(html)
+
+def foo():
+    return a or b or c or d or e or f or g or g or i or j or k
