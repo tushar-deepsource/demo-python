20623ccabb17ac46751da3449b737525717b10b1
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2023-10-26 20:02:55 +0530

    Update django_issues.py

--- django_issues.py
+++ django_issues.py
@@ -8,6 +8,8 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET", "POST"])  # Sensitive
 def current_datetime(request):
     """Doc"""
+
+    
     now = datetime.datetime.now()
     html = "<html><body>It is %s.</body></html>" % now
     return HttpResponse(html)
